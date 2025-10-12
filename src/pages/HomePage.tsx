e43edb3df4c3f76e61a0b7370fedce279664785a
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-10-12 16:08:44 +0300

    refactor: add React import and enhance statistics display in HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 const HomePage = () => {
     interface stats {
@@ -10,6 +11,7 @@ const HomePage = () => {
         { label: "Experience Educators", value: 15 },
     ];
 
+
     return (
         <section className="!p-4 w-full overflow-hidden">
             <div className="md:!px-20  flex flex-col md:flex-row items-center  md:justify-between gap-8">
@@ -18,11 +20,11 @@ const HomePage = () => {
                 </picture>
                 <div className="flex flex-col gap-2 md:w-full w-full">
                     <p className="text-[.9rem]  md:text-[1rem] font-semibold relative  before:absolute before:bottom-[-.2rem] before:left-0 before:h-0.5 before:w-[17.2rem] before:bg-black">Welcome to Little Learners Academy</p>
-                    <h2 className="md:text-[2.9rem] text-[1.8rem] font-bold font[var(--font-raleway)]">Where Young Minds Blossom and <span className="text-[var(--secondary-color)]">Dreams Take Flight.</span></h2>
+                    <h2 className="md:text-[2.9rem] sm:text-[1.8rem] text-2xl font-bold font[var(--font-raleway)]">Where Young Minds Blossom and <span className="text-[var(--secondary-color)]">Dreams Take Flight.</span></h2>
                     <p className="text-[1.2rem] font-medium">Our kinder garden school provides a nurturing and stimulating environment, fostering a love for learning that lasts a lifetime. Join us as we embark on an exciting educational journey together!</p>
                     <div className="flex md:flex-row flex-col md:items-start items-center justify-center gap-8 w-full rounded-lg bg-[var(--quaternary-color)]  shadow-2xl !px-6 !py-4 border-2 border-black">
                         {statistics.map((stat, index) => (
-                            <div key={index} className="flex flex-col items-start justify-center gap-1 !px-6">
+                            <div key={index} className="flex flex-col items-start justify-center gap-1 !px-6 !py-2 relative before:absolute before:top-0 before:right-0 before:h-0.5 before:w-full before:bg-black first:before:hidden md:before:hidden">
                                 <h3 className="text-[2.5rem] font-bold ">{stat.value}+</h3>
                                 <p className="text-[1rem] font-medium">{stat.label}</p>
                             </div>))
