0e68ab44a56f996b7f8f76bb5bffb847408aa2bb
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-10-23 19:39:35 +0300

    feat: add Introduction component for AdmissionsPage with layout and styling

--- src/components/admissions/Introduction.tsx
+++ src/components/admissions/Introduction.tsx
@@ -9,12 +9,12 @@ const Introduction: React.FC = () => {
                 </picture>
                 <div className="flex items-center md:flex-row flex-col justify-center md:justify-between gap-4 md:gap-8 !p-4 md:!mt-0 !mt-12 md:!p-20">
                     <div className='flex flex-col justify-cente items-center md:items-start md:text-left text-center md:w-1/2 w-full  gap-4'>
-                        <div className='bg-white border-2 !p-2 rounded-md'><p className='text-[#333333] text-sm'>Academics</p></div>
+                        <div className='bg-white border-2 !p-2 rounded-md'><p className='text-[#333333] text-sm'>Admission</p></div>
 
-                        <h2 className='md:text-4xl text-[1.5rem] w-[70%] md:w-full font-bold !mt-4' style={{ fontFamily: 'var(--font-raleway)' }}>Nurturing Young Minds for Success</h2>
+                        <h2 className='md:text-4xl text-[1.5rem] w-[70%] md:w-full font-bold !mt-4' style={{ fontFamily: 'var(--font-raleway)' }}>Join Our Family of Young Learners</h2>
                     </div>
                     <div className='flex flex-col md:w-1/2 w-full  gap-4'>
-                        <p className='md:text-[1rem] text-center md:text-start  text-sm text-[#333333]'>Welcome to our Academics page, where we take pride in providing a comprehensive and stimulating educational experience for your child. Our kindergarten school's academic program is thoughtfully designed to foster a love for learning while building a strong foundation of essential skills and knowledge. From language arts and mathematics to science and social studies, our curriculum is carefully crafted to spark curiosity and encourage active exploration.</p>
+                        <p className='md:text-[1rem] text-center md:text-start  text-sm text-[#333333]'>At Little Learners Academy, we welcome you to embark on an exciting educational journey for your child. Our admission process is designed to be transparent, straightforward, and inclusive. Here's a step-by-step guide to joining our school</p>
                     </div>
                 </div>
             </div>
